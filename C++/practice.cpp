84dbf5db6e56be20bad2928748c8acff27f99cca
Sarang Ashok Admane <128445950+SARANG003@users.noreply.github.com>
2024-06-21 18:52:34 +0530

    Add files via upload

---
+++ C++/practice.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int X = 0;
+    cout<<"Enter the number : \n";
+    cin>>X;
+
+    if((X%2)==0)
+    {
+        cout<<"It is a even number"<<"\n";
+    }
+    else
+    {
+        cout<<"It is a odd number "<<"\n";
+    }
+    return 0;
+}
